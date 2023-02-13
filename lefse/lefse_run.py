9a90340a097fc570a1b81d189db7188a23b0248f
JEONGHYEONHAN <125239601+JEONGHYEONHAN@users.noreply.github.com>
2023-02-13 14:16:57 +0900

    Update lefse_run.py

--- lefse/lefse_run.py
+++ lefse/lefse_run.py
@@ -2,7 +2,8 @@
 
 import os,sys,math,pickle
 from lefse.lefse import *
-
+import pandas as pd
+import numpy as np
 def read_params(args):
     parser = argparse.ArgumentParser(description='LEfSe 1.1.01')
     parser.add_argument('input_file', metavar='INPUT_FILE', type=str, help="the input file")
@@ -66,20 +67,26 @@ def lefse_run():
             print("Testing feature",str(nf),": ",feat_name)
             nf += 1
         kw_ok,pv = test_kw_r(cls,feat_values,params['anova_alpha'],sorted(cls.keys()))
+        
+        '''
         if not kw_ok:
             if params['verbose']: print("\tkw ko")
             del feats[feat_name]
             wilcoxon_res[feat_name] = "-"
             continue
+        '''
+
+
+        
         if params['verbose']: print("\tkw ok\t")
 
         if not params['wilc']: continue
         kw_n_ok += 1
         res_wilcoxon_rep = test_rep_wilcoxon_r(subclass_sl,class_hierarchy,feat_values,params['wilcoxon_alpha'],params['multiclass_strat'],params['strict'],feat_name,params['min_c'],params['only_same_subcl'],params['curv'])
-        wilcoxon_res[feat_name] = str(pv) if res_wilcoxon_rep else "-"
+        wilcoxon_res[feat_name] = str(pv) #if res_wilcoxon_rep else "-"
         if not res_wilcoxon_rep:
             if params['verbose']: print("wilc ko")
-            del feats[feat_name]
+            #del feats[feat_name]
         elif params['verbose']: print("wilc ok\t")
 
     if len(feats) > 0:
@@ -100,9 +107,81 @@ def lefse_run():
     outres['cls_means'] = cls_means
     outres['cls_means_kord'] = kord
     outres['wilcox_res'] = wilcoxon_res
-    print("Number of discriminative features with abs LDA score >",params['lda_abs_th'],":",len(lda_res_th))
-    save_res(outres,params["output_file"])
 
+    print("Number of discriminative features with abs LDA score >",params['lda_abs_th'],":",len(lda_res_th))
+    
+      
+    save_res(outres,params["output_file"]+"_total.res") # _filtered.res : original result file, 유의하지 않은(LDA<2 또는 p-value >0.05) taxonomy에 대한 정보는 빈값
+    
+    df = pd.read_csv(params["output_file"]+"_total.res", sep = '\t', header=None, index_col=None)
+    df.columns = ['taxonomy','log mean','class','lda','pvalue']
+    
+    df['lda'] = df['lda'].astype(float)
+    df['pvalue'] = df['pvalue'].astype(float)
+    
+    df.loc[(df['lda']<2)|(df['pvalue']>0.05),['class','lda','pvalue']]=[np.nan,np.nan,'-']
+    print(df)
+    df.to_csv(params["output_file"]+"_filtered.res",sep='\t', index = None, header = None)
+    
+    
+    ##################################################################################################################
+    '''
+    params = read_params(sys.argv)
+    feats,cls,class_sl,subclass_sl,class_hierarchy = load_data(params['input_file'])
+    kord,cls_means = get_class_means(class_sl,feats)
+    wilcoxon_res = {}
+    kw_n_ok = 0
+    nf = 0
+    for feat_name,feat_values in list(feats.items()):
+        if params['verbose']:
+            print("Testing feature",str(nf),": ",feat_name)
+            nf += 1
+        kw_ok,pv = test_kw_r(cls,feat_values,params['anova_alpha'],sorted(cls.keys()))
+        
+
+        if not kw_ok:
+            if params['verbose']: print("\tkw ko")
+            del feats[feat_name]
+            wilcoxon_res[feat_name] = "-"
+            continue
+
+
+        
+        if params['verbose']: print("\tkw ok\t")
+
+        if not params['wilc']: continue
+        kw_n_ok += 1
+        res_wilcoxon_rep = test_rep_wilcoxon_r(subclass_sl,class_hierarchy,feat_values,params['wilcoxon_alpha'],params['multiclass_strat'],params['strict'],feat_name,params['min_c'],params['only_same_subcl'],params['curv'])
+        #wilcoxon_res[feat_name] = str(pv) if res_wilcoxon_rep else "-"
+        if not res_wilcoxon_rep:
+            if params['verbose']: print("wilc ko")
+            del feats[feat_name]
+        elif params['verbose']: print("wilc ok\t")
+
+    if len(feats) > 0:
+        #print("Number of significantly discriminative features:", len(feats), "(", kw_n_ok, ") before internal wilcoxon")
+        if params['lda_abs_th'] < 0.0:
+            lda_res,lda_res_th = dict([(k,0.0) for k,v in feats.items()]), dict([(k,v) for k,v in feats.items()])
+        else:
+            if params['rank_tec'] == 'lda': lda_res,lda_res_th = test_lda_r(cls,feats,class_sl,params['n_boots'],params['f_boots'],params['lda_abs_th'],0.0000000001,params['nlogs'])
+            elif params['rank_tec'] == 'svm': lda_res,lda_res_th = test_svm(cls,feats,class_sl,params['n_boots'],params['f_boots'],params['lda_abs_th'],0.0,params['svm_norm'])
+            else: lda_res,lda_res_th = dict([(k,0.0) for k,v in feats.items()]), dict([(k,v) for k,v in feats.items()])
+    else:
+        #print("Number of significantly discriminative features:", len(feats), "(", kw_n_ok, ") before internal wilcoxon")
+        #print("No features with significant differences between the two classes")
+        lda_res,lda_res_th = {},{}
+    outres = {}
+    outres['lda_res_th'] = lda_res_th
+    outres['lda_res'] = lda_res
+    outres['cls_means'] = cls_means
+    outres['cls_means_kord'] = kord
+    outres['wilcox_res'] = wilcoxon_res
+    
+    print(outres.keys())
+    save_res(outres,params["output_file"]+"_total.res") # _total.res : total result file, 유의한 taxonomy에 대한 정보는 빈값
+
+    '''
+    
 
 if __name__ == '__main__':
     lefse_run()
