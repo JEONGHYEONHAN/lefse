528181a6763312c45724288cc814ff01bebcd8f7
JEONGHYEONHAN <125239601+JEONGHYEONHAN@users.noreply.github.com>
2023-02-13 14:17:30 +0900

    Update lefse_plot_res.py

--- lefse/lefse_plot_res.py
+++ lefse/lefse_plot_res.py
@@ -9,20 +9,31 @@ from collections import defaultdict
 from lefse.lefse import *
 import argparse
 
-colors = ['r','g','b','m','c','y','k','w']
+import seaborn as sns
+import pandas as pd
+colors = sns.color_palette('Set2')+sns.color_palette('Set3')+sns.color_palette('Pastel2')+sns.color_palette('Pastel1')
+mapping = pd.read_csv("/home/qiime2/input/metadata/mapping.txt",sep='\t',index_col=0)
+groups = len(set(mapping[mapping.columns[-1]].to_list()))
+
+while True:
+  if len(colors)<groups : 
+    colors = colors +colors
+  else : break
+  
+#colors = ['r','g','b','m','c','y','k','w']
 
 def read_params(args):
     parser = argparse.ArgumentParser(description='Plot results')
     parser.add_argument('input_file', metavar='INPUT_FILE', type=str, help="tab delimited input file")
     parser.add_argument('output_file', metavar='OUTPUT_FILE', type=str, help="the file for the output image")
-    parser.add_argument('--feature_font_size', dest="feature_font_size", type=int, default=7, help="the file for the output image")
+    parser.add_argument('--feature_font_size', dest="feature_font_size", type=int, default=8, help="the file for the output image")
     parser.add_argument('--format', dest="format", choices=["png","svg","pdf"], default='png', type=str, help="the format for the output file")
     parser.add_argument('--dpi',dest="dpi", type=int, default=72)
     parser.add_argument('--title',dest="title", type=str, default="")
-    parser.add_argument('--title_font_size',dest="title_font_size", type=str, default="12")
-    parser.add_argument('--class_legend_font_size',dest="class_legend_font_size", type=str, default="10")
-    parser.add_argument('--width',dest="width", type=float, default=7.0 )
-    parser.add_argument('--height',dest="height", type=float, default=4.0, help="only for vertical histograms")
+    parser.add_argument('--title_font_size',dest="title_font_size", type=str, default="10")
+    parser.add_argument('--class_legend_font_size',dest="class_legend_font_size", type=str, default="8")
+    parser.add_argument('--width',dest="width", type=float, default=10.0 )
+    parser.add_argument('--height',dest="height", type=float, default=6.0, help="only for vertical histograms")
     parser.add_argument('--left_space',dest="ls", type=float, default=0.2 )
     parser.add_argument('--right_space',dest="rs", type=float, default=0.1 )
     parser.add_argument('--orientation',dest="orientation", type=str, choices=["h","v"], default="h" )
@@ -69,8 +80,8 @@ def plot_histo_hor(path,params,data,bcl,report_features):
     fig = plt.figure(figsize=(params['width'], ints + ht), edgecolor=params['back_color'],facecolor=params['back_color'])
     ax = fig.add_subplot(111,frame_on=False,facecolor=params['back_color'])
     ls, rs = params['ls'], 1.0-params['rs']
-    plt.subplots_adjust(left=ls,right=rs,top=1-head*(1.0-ints/(ints+ht)), bottom=tail*(1.0-ints/(ints+ht)))
-
+    #plt.subplots_adjust(left=ls,right=rs,top=1-head*(1.0-ints/(ints+ht)), bottom=tail*(1.0-ints/(ints+ht)))
+    
     fig.canvas.manager.set_window_title('LDA results')
 
     l_align = {'horizontalalignment':'left', 'verticalalignment':'baseline'}
@@ -91,7 +102,7 @@ def plot_histo_hor(path,params,data,bcl,report_features):
         if len(cls2) > 0: 
             col = colors[cls2.index(v[2])%len(colors)]
         vv = fabs(float(v[3])) * (m*(indcl*2-1)) if bcl else fabs(float(v[3]))
-        ax.barh(pos[i],vv, align='center', color=col, label=lab, height=0.8, edgecolor=params['fore_color'])
+        ax.barh(pos[i],vv, align='center', color=col, label=lab, height=0.8, edgecolor='#595959', linewidth=0.8)
     mv = max([abs(float(v[3])) for v in data['rows']])  
     if report_features:
         print('OTU\tLDA_score\tCLass')
@@ -101,13 +112,16 @@ def plot_histo_hor(path,params,data,bcl,report_features):
         indcl = cls.index(data['rows'][i][2])
         if params['n_scl'] < 0: rr = r[0]
         else: rr = ".".join(r[0].split(".")[-params['n_scl']:])
-        if len(rr) > params['max_feature_len']: rr = rr[:params['max_feature_len']/2-2]+" [..]"+rr[-params['max_feature_len']/2+2:]
+        if len(rr) > params['max_feature_len']: rr = rr[:params['max_feature_len']//2-2]+" [..]"+rr[-params['max_feature_len']//2+2:]
+        #if len(rr) > params['max_feature_len']: rr = rr[:params['max_feature_len']/2-2]+" [..]"+rr[-params['max_feature_len']/2+2:]
         if m*(indcl*2-1) < 0 and bcl: ax.text(mv/40.0,float(i)-0.3,rr, l_align, size=params['feature_font_size'],color=params['fore_color'])
         else: ax.text(-mv/40.0,float(i)-0.3,rr, r_align, size=params['feature_font_size'],color=params['fore_color'])
     ax.set_title(params['title'],size=params['title_font_size'],y=1.0+head*(1.0-ints/(ints+ht))*0.8,color=params['fore_color'])
 
     ax.set_yticks([])
-    ax.set_xlabel("LDA SCORE (log 10)")
+    ax.set_xlabel("LDA SCORE (log 10)",fontsize=params['feature_font_size']+1)
+    ax.tick_params(axis='x', labelsize= params['feature_font_size'])
+
     ax.xaxis.grid(True)
     xlim = ax.get_xlim()
     if params['autoscale']: 
@@ -115,7 +129,12 @@ def plot_histo_hor(path,params,data,bcl,report_features):
         if len(ran) > 1 and len(ran) < 100:
             ax.set_xticks(arange(xlim[0],xlim[1]+0.0001,min(xlim[1]+0.0001,round(round((abs(xlim[0])+abs(xlim[1]))/10,4)*100,0)/100)))
     ax.set_ylim((pos[0]-1,pos[-1]+1))
-    leg = ax.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=5, borderaxespad=0., frameon=False,prop={'size':params['class_legend_font_size']})
+    
+    leg = ax.legend(ncol=1, bbox_to_anchor=(1,1),loc='upper left', prop={'size':params['class_legend_font_size']})
+    #leg = ax.legend(ncol=5, borderaxespad=0., frameon=False, prop={'size':params['class_legend_font_size']})
+    #leg = ax.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=5, borderaxespad=0., frameon=False,prop={'size':params['class_legend_font_size']})
+    #leg = ax.legend(ncol=4, frameon=False,prop={'size':params['class_legend_font_size']})
+
 
     def get_col_attr(x):
                 return hasattr(x, 'set_color') and not hasattr(x, 'set_facecolor')
@@ -124,8 +143,8 @@ def plot_histo_hor(path,params,data,bcl,report_features):
     for o in ax.findobj(get_col_attr):
                 o.set_color(params['fore_color'])
 
-    
-    plt.savefig(path,format=params['format'],facecolor=params['back_color'],edgecolor=params['fore_color'],dpi=params['dpi'])
+    plt.tight_layout()
+    plt.savefig(path,format=params['format'],facecolor=params['back_color'],edgecolor=params['fore_color'],dpi=params['dpi'],bbox_inches='tight')
     plt.close()
 
 def plot_histo_ver(path,params,data,report_features):
@@ -154,18 +173,21 @@ def plot_histo_ver(path,params,data,report_features):
         added.append(str(v[2])) 
         col = colors[indcl%len(colors)]
         vv = fabs(float(v[3])) 
-        ax.bar(pos[i],vv, align='center', color=col, label=lab)
+        ax.bar(pos[i],vv, align='center', color=col, label=lab, linewidth = 0)
     if report_features:
         print('OTU\tLDA_score\tCLass')
         for i in out_data:
             print('%s\t%s\t%s' %(i, out_data[i][0], out_data[i][1]))
     xticks(pos,nam,rotation=-20, ha = 'left',size=params['feature_font_size'])  
+
     ax.set_title(params['title'],size=params['title_font_size'])
     ax.set_ylabel("LDA SCORE (log 10)")
     ax.yaxis.grid(True) 
     a,b = ax.get_xlim()
     dx = float(len(pos))/float((b-a))
     ax.set_xlim((0-dx,max(pos)+dx)) 
+    
+    
     plt.savefig(path,format=params['format'],facecolor=params['back_color'],edgecolor=params['fore_color'],dpi=params['dpi'])
     plt.close() 
 
